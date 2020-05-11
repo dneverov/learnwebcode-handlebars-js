201571f12a62c1a3f5f18ad9d7c8586e87ddf9ee
Denis Neverov <denis.neverov@gmail.com>
2020-05-11 03:27:01 +0300

    Use NPM Webpack

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ A working example from https://www.youtube.com/watch?v=wSNa5b1mS5Y
 https://github.com/dneverov/learnwebcode-json-ajax
 
 ## TODO
-- [ ] Add an initial branch
-- [ ] npm & webpack
+- [x] Add an initial branch
+- [x] npm & webpack
+
+      npm install webpack -g
+      npm install
+      webpack
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+var myTemplate = require("./myTemplate.hbs");
+
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
 ourRequest.onload = function() {
   if (ourRequest.status >= 200 && ourRequest.status < 400) {
-    // This is where we'll do something with the retrieved data
     var data = JSON.parse(ourRequest.responseText);
     createHTML(data);
   } else {
@@ -16,22 +17,7 @@ ourRequest.onerror = function() {
 
 ourRequest.send();
 
-Handlebars.registerHelper("calclulateAge", function(birthYear){
-  var age = new Date().getFullYear() - birthYear;
-
-  if (age > 0){
-    age += " years old";
-  } else {
-    age = "Less than a year old";
-  }
-  return age;
-});
-
-function createHTML(petsData){
-  var rawTemplate = document.getElementById("petsTemplate").innerHTML;
-  var compiledTemplate = Handlebars.compile(rawTemplate);
-  var ourGeneratedHTML = compiledTemplate(petsData);
-
+function createHTML(petsData) {
   var petsContainer = document.getElementById("pets-container");
-  petsContainer.innerHTML = ourGeneratedHTML;
+  petsContainer.innerHTML = myTemplate(petsData);
 }
